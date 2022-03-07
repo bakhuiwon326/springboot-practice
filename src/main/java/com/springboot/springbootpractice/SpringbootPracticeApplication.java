c472219350d4833d402b060d60f3ea1ddda8ec1a
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2022-03-08 01:56:10 +0900

    properties 공부, @SpringBootApplication 이용

--- src/main/java/com/springboot/springbootpractice/SpringbootPracticeApplication.java
+++ src/main/java/com/springboot/springbootpractice/SpringbootPracticeApplication.java
@@ -2,11 +2,13 @@ package com.springboot.springbootpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
 public class SpringbootPracticeApplication {
 
     public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(SpringbootPracticeApplication.class);
         SpringApplication.run(SpringbootPracticeApplication.class, args);
     }
 
